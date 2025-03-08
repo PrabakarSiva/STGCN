7a3629fd2fed6617b6a030a3b100d553631f7262
Siva Prabakar <prabakas@hpc3-gpu-24-07.local>
2025-03-08 15:39:01 -0800

    added urban1 dataset support

--- script/dataloader.py
+++ script/dataloader.py
@@ -7,8 +7,17 @@ import torch
 def load_adj(dataset_name):
     dataset_path = './data'
     dataset_path = os.path.join(dataset_path, dataset_name)
-    adj = sp.load_npz(os.path.join(dataset_path, 'adj.npz'))
-    adj = adj.tocsc()
+    if dataset_name == 'urban1':
+        adj = pd.read_csv(os.path.join(dataset_path, 'adj.csv'), header=None).replace([np.inf, -np.inf], np.nan).interpolate(method='linear')
+        print(adj.isin([np.inf, -np.inf, np.nan]).sum().sum())
+        adj = adj.interpolate(method='linear', axis=1)
+        print(adj.isin([np.nan]).sum().sum())
+        print("adj df shape: ", adj.shape)
+        adj = sp.csc_matrix(adj.values)
+        print("adj shape: ", adj.shape)
+    else:
+        adj = sp.load_npz(os.path.join(dataset_path, 'adj.npz'))
+        adj = adj.tocsc()
     
     if dataset_name == 'metr-la':
         n_vertex = 207
@@ -16,13 +25,15 @@ def load_adj(dataset_name):
         n_vertex = 325
     elif dataset_name == 'pemsd7-m':
         n_vertex = 228
+    elif dataset_name == 'urban1':
+        n_vertex = 480
 
     return adj, n_vertex
 
 def load_data(dataset_name, len_train, len_val):
     dataset_path = './data'
     dataset_path = os.path.join(dataset_path, dataset_name)
-    vel = pd.read_csv(os.path.join(dataset_path, 'vel.csv'))
+    vel = pd.read_csv(os.path.join(dataset_path, 'vel.csv')).interpolate(method='linear')
 
     train = vel[: len_train]
     val = vel[len_train: len_train + len_val]
